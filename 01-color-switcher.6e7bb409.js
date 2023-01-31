84bb256dd5689cbc604eb938daa06900274fa4d7
OleksandrStad <112192045+OleksandrStad@users.noreply.github.com>
2023-01-31 21:57:20 +0000

    Deploying to gh-pages from @ OleksandrStad/goit-js-hw-09@ba3c907299afe390258b4bb2ca3ebb5c574523fe 🚀

---
+++ 01-color-switcher.6e7bb409.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(t){console.log("mnbnv"),o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(t){console.log("!!!!"),clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.6e7bb409.js.map
